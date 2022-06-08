e6d84ef3528ec48d50b511bb22570ed29ef7dbf0
nicovalverdi <104444803+nicovalverdi@users.noreply.github.com>
2022-06-08 11:48:57 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,14 @@
+
+   function inclusive()
+  {
+      document.body.style.backgroundColor= "black";
+
+      document.getElementById("header").style.backgroundColor = "black";
+      document.getElementById("row").style.opacity = "80%";
+      document.getElementById("footer").style.backgroundColor = "black";
+
+}
+
+    function copy() {
+        alert("Autor: Nicolas Agustin Valverdi. EMAIL: nicov35@hotmail.com");
+        }
---
+++ jscontacto/scriptcontacto.js
@@ -0,0 +1,12 @@
+function inclusive(){
+
+  document.body.style.backgroundColor= "black";
+
+  document.getElementById("header").style.backgroundColor = "black";
+  document.getElementById("form").style.backgroundColor = "#355852";
+  document.getElementById("footer").style.backgroundColor = "black";
+
+}
+function copy() {
+    alert("Autor: Nicolas Agustin Valverdi. EMAIL: nicov35@hotmail.com");
+    }
